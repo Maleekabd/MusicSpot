fdeef2b5994bd85e1eb35b436c73f7df76e31ce3
Malik <malikabdurrasyid86@gmail.com>
2023-08-20 16:25:31 +0700

    ada perubahan

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -15,7 +15,7 @@ const sidebar = ({ children }) => {
                     <ul className="mt-6 space-y-1">
                         <li>
                             <a
-                                href=""
+                                href="https://www.instagram.com/__malikabd"
                                 className="block rounded-lg bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700"
                             >
                                 General
@@ -50,7 +50,7 @@ const sidebar = ({ children }) => {
                                 <ul className="mt-2 space-y-1 px-4">
                                     <li>
                                         <a
-                                            href=""
+                                            href="https://www.instagram.com/__malikabd"
                                             className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
                                         >
                                             Banned Users
@@ -59,7 +59,7 @@ const sidebar = ({ children }) => {
 
                                     <li>
                                         <a
-                                            href=""
+                                            href="https://www.instagram.com/__malikabd"
                                             className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
                                         >
                                             Calendar
@@ -71,7 +71,7 @@ const sidebar = ({ children }) => {
 
                         <li>
                             <a
-                                href=""
+                                href="https://www.instagram.com/__malikabd"
                                 className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
                             >
                                 Invoices
@@ -106,7 +106,7 @@ const sidebar = ({ children }) => {
                                 <ul className="mt-2 space-y-1 px-4">
                                     <li>
                                         <a
-                                            href=""
+                                            href="https://www.instagram.com/__malikabd"
                                             className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
                                         >
                                             Details
@@ -115,7 +115,7 @@ const sidebar = ({ children }) => {
 
                                     <li>
                                         <a
-                                            href=""
+                                            href="https://www.instagram.com/__malikabd"
                                             className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
                                         >
                                             Security
@@ -139,7 +139,7 @@ const sidebar = ({ children }) => {
                 </div>
 
                 <div className="sticky inset-x-0 bottom-0 border-t border-gray-100">
-                    <a href="#" className="flex items-center gap-2 bg-white p-4 hover:bg-gray-50">
+                    <a href="https://www.instagram.com/__malikabd" className="flex items-center gap-2 bg-white p-4 hover:bg-gray-50">
                         <img
                             alt="Man"
                             src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
